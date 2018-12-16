c8da5e2a11cd7ef521a2d86c167b31794c9b3937
Matheus dos Reis <matheus.reis@nobugs.com.br>
2018-12-16 19:15:49 -0200

    App totally set and Bluetooth connection

--- App.js
+++ App.js
@@ -1,11 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  ToolbarAndroid
+} from "react-native";
+
+import DeviceList from "./src/components/DevicesList";
+
+import BluetoothSerial from "react-native-bluetooth-serial";
+import { Icon, Divider, Header } from "react-native-elements";
 
 export default class App extends React.Component {
+  btManager = BluetoothSerial;
+  constructor(props) {
+    super(props);
+    this.state = {
+      btEnabled: false
+    };
+  }
+  async componentDidMount() {
+    const en = await this.btManager.isEnabled();
+    this.setState({
+      btEnabled: en
+    });
+    this.btManager.on("bluetoothEnabled", () => {
+      this.setState({ btEnabled: true });
+    });
+    this.btManager.on("bluetoothDisabled", () => {
+      this.setState({ btEnabled: false });
+    });
+  }
+  toggleBt = e => {
+    if (this.state.btEnabled) this.btManager.disable();
+    else this.btManager.enable();
+  };
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <StatusBar hidden/>
+        <Header
+          outerContainerStyles={{ height: 56 }}
+          backgroundColor={"#0F728F"}
+          centerComponent={{
+            text: "Natuino",
+            style: { color: "white", fontSize: 20 }
+          }}
+          rightComponent={{
+            icon: this.state.btEnabled ? "bluetooth" : "bluetooth-disabled",
+            onPress: () => this.toggleBt(),
+            color: "white",
+            size: 25,
+            underlayColor: "transparent"
+          }}
+        />
+        <DeviceList bt={this.btManager} />
       </View>
     );
   }
@@ -14,8 +65,12 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  toolbar: {
+    height: 60,
+    backgroundColor: "#0F728F"
+  }
 });
--- src/components/DevicesList.js
+++ src/components/DevicesList.js
@@ -1,18 +1,67 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { ListView } from "react-native"
+import {
+  FlatList,
+  View,
+  Text,
+  StyleSheet,
+  ToastAndroid
+} from "react-native";
+import { Button, Card, Icon } from "react-native-elements";
 
 export default class DevicesList extends Component {
-    constructor(props){
-        super(props)
-        const ds = new ListView.DataSource()
-        this.state = {
-            devices : ds
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      devices: []
+    };
+  }
+  async componentDidMount() {
+    if(! await this.props.bt.isConnected()) {
+      ToastAndroid.showWithGravityAndOffset("Not connected to any device",2000,ToastAndroid.BOTTOM,0,25)
     }
-    render(){
-        return (
-            <ListView dataSource={this.state.devices}/>
-        )
-    }
+    this.scanDevice();
+  }
+  scanDevice = async e => {
+    const list = await this.props.bt.discoverUnpairedDevices();
+    this.setState({ devices: list });
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          rounded
+          color={"white"}
+          backgroundColor={"#0F728F"}
+          buttonStyle={{width: "50%"}}
+          textStyle={{height: "80%", flex: 1, alignContent: "center"}}
+          onPress={this.scanDevice}
+          title={"Scan"}
+        />
+        <FlatList
+          data={this.state.devices}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Card key={item.id} title={item.name}>
+              <Text key={item.id}> ID: {item.id}</Text>
+              <Button key={item.id}
+                backgroundColor={"#0F728F"} 
+                icon={{name: "bluetooth"}} 
+                title="Connect"
+              />
+            </Card>
+          )}
+        />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: 10
+  }
+});
